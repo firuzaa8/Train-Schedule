9f6e31aea69a595a0eb801523fde0e50f6bee082
Firuza Abramyan <firuzaa8@gmail.com>
2019-11-22 13:45:03 -0500

    without css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,3 @@
-//<script src="https://cdn.jsdelivr.net/momentjs/2.12.0/moment.min.js"></script>
-
-//<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js"></script>
-
-//<script src="https://www.gstatic.com/firebasejs/7.4.0/firebase-database.js"></script>
-
 var firebaseConfig = {
     apiKey: "AIzaSyCsSOlvMib1DpSEwmsB1cNcxmyBbfWB5Io",
     authDomain: "local-train-schedule-a549d.firebaseapp.com",
@@ -14,35 +8,79 @@ var firebaseConfig = {
     appId: "1:20066775228:web:b13968682ba362fc76a0b9",
     measurementId: "G-M484QCB86D"
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-var database = firebase.database();
 
-var trainName = "";
-var destination = "";
-var frequency = 0;
-var firstTrainTime = "";
+$(document).ready(function () {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database();
 
-$("#add-user").on("click", function (event) {
-    event.preventDefault();
-    trainName = $("#InputTextName").val().trim();
-    destination = $("#InputTextDest").val().trim();
-    frequency = $("#InputTextFreq").val().trim();
-    firstTrainTime = $("#InputTextTime").val().trim();
+    var trainName = "";
+    var destination = "";
+    var frequency = 0;
+    var firstTrainTime = "08:00";
 
-    database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        frequency: frequency,
-        firstTrainTime: firstTrainTime,
+ 
+
+
+    $("#add-train").on("click", function (event) {
+        event.preventDefault();
+        trainName = $("#InputTextName").val().trim();
+        destination = $("#InputTextDest").val().trim();
+        frequency = $("#InputTextFreq").val().trim();
+        firstTrainTime = $("#InputTextTime").val().trim();
+
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            frequency: frequency,
+            firstTrainTime: firstTrainTime,
+        });
+
+        $("#InputTextName").val("");
+        $("#InputTextDest").val("");
+        $("#InputTextFreq").val("");
+        $("#InputTextTime").val("");
+
     });
-});
-database.ref().on("child_added", function (snapshot) {
-    var sv = snapshot.val();
-    console.log(sv.trainName);
-    console.log(sv.destination);
-    console.log(sv.frequency);
-    console.log(sv.firstTrainTime);
 
+   
+    database.ref().on("child_added", function (snapshot) {
+        var sv = snapshot.val();
+
+        var currentTime = moment();
+        var firstArrival = moment(sv.firstTrainTime, "HH:mm");
+
+
+        var diffInMinutes = currentTime.diff(firstArrival, "minutes");
+        console.log(diffInMinutes);
+
+        var remainder = diffInMinutes % sv.frequency;
+        console.log(remainder);
+
+        var minutesToNextArrival = sv.frequency - remainder;
+        console.log(minutesToNextArrival);
+
+        var nextArrival = currentTime.add(minutesToNextArrival, "minutes");
+
+        var nextArrivalString = nextArrival.format("HH:mm")
+
+
+        $("#main").append("<div class='row'>" +
+            "<div class='col-sm'> " + sv.trainName + " </div>" +
+            "<div class='col-sm'> " + sv.destination + " </div>" +
+            "<div class='col-sm('> " + sv.frequency + " </div>" +
+            "<div class='col-sm'> " + nextArrivalString + " </div>" +
+            "<div class='col-sm'> " + minutesToNextArrival + " </div>" +
+
+
+            "</div>");
+
+
+
+
+
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
 
 });
